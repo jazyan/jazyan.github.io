1b2f5b9e4155a8bb0f973f856a3e0da5812b43a4
jasmine <jazyan11@gmail.com>
2019-05-11 12:08:48 -0700

    add files

---
+++ particles.js
@@ -0,0 +1,10 @@
+ var particles = Particles.init({
+    selector: '.background',
+    maxParticles: 300,
+    sizeVariations: 2,
+    speed: 0.5,
+    color: ['#FFFFFF', '#00FFFF', '#FF7F50'],
+    minDistance: 120,
+    connectParticles: true,
+    responsive: null,
+});
