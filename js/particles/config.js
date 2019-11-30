b36c1fb24d36bc00706c630465e0ddebecec1352
jasmine <jazyan11@gmail.com>
2019-11-30 12:14:58 -0800

    fix padding and margins

--- js/particles/config.js
+++ js/particles/config.js
@@ -1,10 +1,12 @@
- var particles = Particles.init({
-    selector: '.background',
-    maxParticles: 150,
-    sizeVariations: 5,
-    speed: 0.25,
-    color: ['#FFFFFF', '#00FFFF', '#FF7F50'],
-    minDistance: 120,
-    connectParticles: true,
-    responsive: null,
-});
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        maxParticles: 150,
+        sizeVariations: 5,
+        speed: 0.25,
+        color: ['#FFFFFF', '#00FFFF', '#FF7F50'],
+        minDistance: 120,
+        connectParticles: true,
+        responsive: null,
+    });
+};
