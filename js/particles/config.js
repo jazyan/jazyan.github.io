81f9abb48327cb4aa2b957f83831dbb6c408cc01
jasmine <jazyan11@gmail.com>
2019-11-30 11:55:47 -0800

    reorg

--- js/particles/config.js
+++ js/particles/config.js
@@ -2,7 +2,7 @@
     selector: '.background',
     maxParticles: 150,
     sizeVariations: 5,
-    speed: 0.5,
+    speed: 0.25,
     color: ['#FFFFFF', '#00FFFF', '#FF7F50'],
     minDistance: 120,
     connectParticles: true,
